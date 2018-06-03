02f01f8fa82f16162f21413be24db6e41e77d798
node-toso-api <alisonsmithaccy@gmail.com>
2018-06-03 09:23:04 +0100

    added socket.io handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
-const express = require("express")
-const app = express()
 const path = require('path')
+const http = require('http')
+const express = require("express")
+const socketIO =require('socket.io')
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 
+var app = express()
+var server = http.createServer(app)
+var io = socketIO(server)
+
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('Disconnect', () => {
+    console.log('User disconnected from client');
+  })
+})
+
+
+
+server.listen(port, () => {
   console.log(`Started on port ${port}`)
 })
